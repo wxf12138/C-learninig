882d309c93450ac5735aaf027655e6c49ac71d12
wxf <1623285873@qq.com>
2025-03-28 07:09:38 -0400

    修改了CComplex.cc

--- 001_cppBase/CComplex.cc
+++ 001_cppBase/CComplex.cc
@@ -22,30 +22,41 @@ class CComplex
             return CComplex(re - c.re, im - c.im);
         }
 
-        CComplex operator++ ()
+        CComplex& operator++ ()
         {
             re++;
+            im++;
             return *this;
         } // 前置
 
         CComplex operator++ (int)
         {
+            //CComplex temp = *this; // 先保存当前对象
+            //re++;
+            //im++;
+            //return temp; // 返回保存的对象
             return CComplex(re++, im++);
         }// 后置
+
+        void operator+= (const CComplex& c)
+        {
+            re += c.re;
+            im += c.im;
+        }
 
         void show (){
             cout << re << " + " << im << "i" << endl;
         }
 };
 
-CComplex operator+(const CComplex &d, const CComplex &c)
+CComplex operator+(const CComplex &d, const CComplex &c)//全局运算符重载
 {
     return CComplex(d.re + c.re, d.im + c.im );
 }
 
 ostream &operator<<(ostream &out, const CComplex &c)
 {
-    out << c.re << " + " << c.im << "i" << endl;
+    out << c.re << " + " << c.im << "i " ;
     return out;
 }
 
@@ -59,14 +70,15 @@ int main()
 {
     CComplex c1(1, 2), c2(3, 4);
     CComplex c3 = c1 + c2;
-    CComplex c4 = 20 + c1;
+    CComplex c4 = 20 + c1;//全局运算符重载
     c3.show();
     c4.show();
     CComplex c5 = c4++;
     c5.show();
     CComplex c6 = ++c5;
     c6.show(); 
+    c1 += c2;
 
-    cout << c3 << c4 << c5 << c6 << endl;
+    cout << c1 << c3 << c4 << c5 << c6 << endl;
 
 }
